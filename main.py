deed59f2b14d69d7aac082565aca6e07d7e8a4cd
Paul Dinh <105019450+pd19184@users.noreply.github.com>
2022-06-01 22:30:45 +0000

    I have finished  my code

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from random import randint
-player = ("rock (r), paper(p), scissors(s)")
-print(player, 'vs', end=' ')
+player = input ('rock (r), paper (p) or scissors (s)?')
+print(player, 'vs', end='')
 chosen = randint(1,3)
 #print(chosen)
 print(chosen)
-
 if chosen == 1:
   computer = 'r'
-
 elif chosen == 2:
   computer = 'p'
-
 else:
   computer = 's'
 print(computer)
 if player == computer:
   print('DRAW!')
+
 elif player == 'r' and computer == 's':
-  print('Player Wins')
+  print('Player wins')
+
 elif player == 'r' and computer == 'p':
-  print('Computer Wins')
+  print('Computer wins!')
+
 elif player == 'p' and computer == 'r':
-  print('Player Wins')
+  print('Player wins!')
+
 elif player == 'p' and computer == 's':
-  print('Computer Wins')
+  print('Computer wins!')
+
